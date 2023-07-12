67b6a741c42e5de31e05fa0d977a1f0026633ee4
amisha <amishaadwani0023@gmail.com>
2023-07-13 01:06:17 +0530

    default values

--- src/App.js
+++ src/App.js
@@ -1,44 +1,11 @@
+import React from 'react';
+import FirstNav from './components/FirstNav';
 
-// import './App.css';
-// import  Button  from 'react-bootstrap/Button';
-// import  Navbar  from 'react-bootstrap/Navbar';
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <Navbar >Item1</Navbar>
-//         <Navbar >Item1</Navbar>
-//         <Navbar >Item1</Navbar>
-//         <Navbar >Item1</Navbar>
-
-//         <Button >Click Me</Button>
-  
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-
-function ColorSchemesExample() {
+export default function App() {
   return (
-    <>
-      <Navbar bg="dark" data-bs-theme="dark">
-        <Container>
-          <Navbar.Brand href="#home">TextUtils</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#about">About</Nav.Link>
-          </Nav>
-        </Container>
-      </Navbar>
-      
-    </>
-  );
+    <div>
+     <FirstNav />
+    </div>
+  )
 }
-
-export default ColorSchemesExample;
