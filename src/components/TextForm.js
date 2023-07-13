34994500bf5608a97b87b0c7db8bffa7483f90a4
amisha <amishaadwani0023@gmail.com>
2023-07-13 19:40:19 +0530

    hooks introduced

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,14 +1,34 @@
-import Form from 'react-bootstrap/Form';
+import Form from "react-bootstrap/Form";
+import { useState } from "react";
+import Button from "react-bootstrap/Button";
 
 function TextForm(props) {
+  function handleChange(event) {
+    setText(event.target.value);
+  }
+
+  function handleClick() {
+    let newText = text.toUpperCase();
+    setText(newText);
+  }
+
+  const [text, setText] = useState("Enter your text");
   return (
     <>
-    <Form>
-      <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-        <Form.Label>{props.heading}</Form.Label>
-        <Form.Control as="textarea" rows={4} />
-      </Form.Group>
-    </Form>
+      <Form>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+          <Form.Label>{props.heading}</Form.Label>
+          <Form.Control
+            as="textarea"
+            value={text}
+            onChange={handleChange}
+            rows={4}
+          />
+        </Form.Group>
+        <Button variant="primary" onClick={handleClick}>
+          UpperCase
+        </Button>
+      </Form>
     </>
   );
 }
